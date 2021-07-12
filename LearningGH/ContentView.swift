c7de0b11e72ede59b0d133289c1ff3e5cf055060
Lazlow <laidoffprod@gmail.com>
2021-07-12 14:59:32 -0700

    Initial Commit

---
+++ LearningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningGH
+//
+//  Created by aaron beaubien on 7/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by aaron beaubien on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
